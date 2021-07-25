2c9088d38223ce23c51df9017f6e5ef30657b9af
user <user@PC00>
2021-07-25 14:45:46 +0900

    아이디 저장 구현완료

--- 3rd_project/src/main/java/com/myjeju/interceptor/AuthInterceptor.java
+++ 3rd_project/src/main/java/com/myjeju/interceptor/AuthInterceptor.java
@@ -4,6 +4,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
 import com.myjeju.vo.SessionVO;
@@ -30,4 +31,6 @@ public class AuthInterceptor extends HandlerInterceptorAdapter {
 		
 		return true;
 	}
+	
+	
 }
