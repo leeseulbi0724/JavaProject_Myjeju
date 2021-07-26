a49ce21b950bffffee05dde196bbb7dfe7ad47d7
jsggo2001 <jsggo2001@naver.com>
2021-07-26 13:02:30 +0900

    상세예약부분 버그 디버깅

--- 3rd_project/src/main/java/com/myjeju/service/ReservationService.java
+++ 3rd_project/src/main/java/com/myjeju/service/ReservationService.java
@@ -11,11 +11,12 @@ import com.myjeju.vo.RoomVO;
 public interface ReservationService {
 	
 	String getCurrentName(String hid);
-	ArrayList<RoomVO> searchroom(String start, String end ,String hdid);
-	ArrayList<RoomVO> notavails(String start, String end ,String hdid);
 	HouseVO gethouse(String hid);
 	HDetailVO getdetail(String hdid);
 	ArrayList<RoomImgVO> gethimg(String hdid);
 	boolean setreservation(MReservationVO vo);
 	boolean updateavail(String roomid,String f_dated,String s_dated);
+	ArrayList<RoomVO> searchroom(String start, String end ,String hdid);
+	ArrayList<RoomVO> searchroom_each(String start, String end ,String roomid);
+	ArrayList<RoomVO> notavails(String start, String end ,String hdid);
 }
